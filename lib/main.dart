247940fd691fff3ef4515ebb7a5a2921bd8d4235
Nuno Barreto <nuno.barreto@inventsys.ch>
2020-12-01 10:05:19 +0000

    Move main app code into a StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Container(),
+      ),
+      
+    );
+  }
 }
