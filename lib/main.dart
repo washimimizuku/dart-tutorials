52b7f6dbcc32b48ec55e848b40c50214e612eeb3
Nuno Barreto <nuno.barreto@inventsys.ch>
2020-12-12 18:06:51 +0100

    Create AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
+  MaterialApp(
+    home: BallPage(),
+  ),
+);
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Ask Me Anything'),
+
+      ),
+      body: Container(
+
       ),
     );
+  }
+}
