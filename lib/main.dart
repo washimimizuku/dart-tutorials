e57aa2f173a5b45625b953d5b520079ebbd64151
Nuno Barreto <nuno.barreto@inventsys.ch>
2020-12-12 17:51:53 +0100

    Make dice change randomly when pressed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,18 +16,32 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace () {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-  int leftDiceNumber = 1;
-  int rightDiceNumber = 3;
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('Left button was pressed');
+                setState(() {
+                  changeDiceFace();
+                });
               },
               child: Image.asset('assets/images/dice$leftDiceNumber.png')
             ),
@@ -34,7 +49,9 @@ class DicePage extends StatelessWidget {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('Right button was pressed');
+                setState(() {
+                  changeDiceFace();
+                });
               },
               child: Image.asset('assets/images/dice$rightDiceNumber.png')
             ),
