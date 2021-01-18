a61fbf7489dd2b79dedbd1d5ede1daec42c79fee
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-01-18 17:45:55 +0100

    Use database context on heroes controller

--- lib/controller/heroes_controller.dart
+++ lib/controller/heroes_controller.dart
@@ -1,26 +1,26 @@
 import 'package:aqueduct/aqueduct.dart';
 import 'package:heroes/heroes.dart';
+import 'package:heroes/model/hero.dart';
 
 class HeroesController extends ResourceController {
   HeroesController(this.context);
 
   final ManagedContext context;
-  final _heroes = [
-    {'id': 11, 'name': 'Mr. Nice'},
-    {'id': 12, 'name': 'Narco'},
-    {'id': 13, 'name': 'Bombasto'},
-    {'id': 14, 'name': 'Celeritas'},
-    {'id': 15, 'name': 'Magneta'},  
-  ];
 
   @Operation.get()
   Future<Response> getAllHeroes() async {
-    return Response.ok(_heroes);
+    final heroQuery = Query<Hero>(context);
+    final heroes = await heroQuery.fetch();
+
+    return Response.ok(heroes);
   }
 
   @Operation.get('id')
   Future<Response> getHeroByID(@Bind.path('id') int id) async {
-    final hero = _heroes.firstWhere((hero) => hero['id'] == id, orElse: () => null);
+    final heroQuery = Query<Hero>(context)
+      ..where((h) => h.id).equalTo(id);
+
+    final hero = await heroQuery.fetchOne();
   
     if (hero == null) {
       return Response.notFound();
