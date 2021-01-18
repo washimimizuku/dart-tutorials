1e64698d83485eb258d35f014970d5fddbdfa1b7
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-01-18 18:17:16 +0100

    Add OAuth 2.0

--- lib/channel.dart
+++ lib/channel.dart
@@ -1,3 +1,7 @@
+import 'package:aqueduct/managed_auth.dart';
+import 'package:heroes/model/user.dart';
+import 'package:heroes/controller/register_controller.dart';
+
 import 'controller/heroes_controller.dart';
 import 'heroes.dart';
 
@@ -7,6 +11,9 @@ import 'heroes.dart';
 /// database connections. See http://aqueduct.io/docs/http/channel/.
 class HeroesChannel extends ApplicationChannel {
   ManagedContext context;
+
+  AuthServer authServer;
+
   /// Initialize services in this method.
   ///
   /// Implement this method to initialize services, read values from [options]
@@ -28,6 +35,9 @@ class HeroesChannel extends ApplicationChannel {
     );
 
     context = ManagedContext(dataModel, persistentStore);
+
+    final authStorage = ManagedAuthDelegate<User>(context);
+    authServer = AuthServer(authStorage);
   }
 
   /// Construct the request channel.
@@ -41,8 +51,17 @@ class HeroesChannel extends ApplicationChannel {
     final router = Router();
 
     router
+      .route('/auth/token')
+      .link(() => AuthController(authServer));
+
+    router
       .route('/heroes/[:id]')
+      .link(() => Authorizer.bearer(authServer))
       .link(() => HeroesController(context));
+
+    router
+      .route('/register')
+      .link(() => RegisterController(context, authServer));
 
     // Prefer to use `link` instead of `linkFunction`.
     // See: https://aqueduct.io/docs/http/request_controller/
